62285667aedf32d567bb7fb66482baa2fbcd8b79
Matheus Antonio <matheus.antonio208@hotmail.com>
2021-11-28 15:17:42 -0300

    a

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -6,14 +6,17 @@ import { importCategoryController } from "../modules/cars/useCases/importCategor
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
 const upload = multer({
   dest: "./tmp",
 });
+
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
+  console.log("as");
   return listCategoriesController.handle(request, response);
 });
 
